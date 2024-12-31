6f7cf6c16bbaf26de04d8de2ac2137c7d3959132
JerPete <peteshtc101@gmail.com>
2024-12-31 00:29:24 +0000

    finished project and solution

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_of_words = word_count(text)
+    char_counts = char_count(text)
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_of_words} words found in the document")
-    print(char_count(text))
+    # Convert the character counts dictionary to a list of dictionaries
+    char_list = [{"char": key, "count": value} for key, value in char_counts.items()]
     
+    # Sort the list of dictionaries by 'count' in descending order
+    char_list.sort(key=lambda x: x["count"], reverse=True)
+    
+    # Print the sorted character list without the "char" and "count" labels
+    for item in char_list:  # Iterate through the sorted list
+        print(f"The '{item['char']}' character was found {item['count']} times") 
+
+    print("--- End report ---")
+
 
 def word_count(text):
      words = text.split()
@@ -17,13 +29,16 @@ def get_book_text(path):
     
 def char_count(text):
     char_dic = {}
-    text = text.lower()
+    text = text.lower()  
     for char in text:
-        if char in char_dic:
-            char_dic[char] +=1
-        else:
-            char_dic[char] = 1
-    return char_dic
+        if char.isalpha():  
+            if char in char_dic:
+                char_dic[char] += 1  
+            else:
+                char_dic[char] = 1  
+    return char_dic 
+
+
 
 
 
